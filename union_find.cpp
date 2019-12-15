113453d0f4256e9c325f2f452e51548b751424a9
João Marcos <marcospb19@hotmail.com>
2019-12-15 11:46:24 -0300

    Small changes to union_find.cpp

--- union_find.cpp
+++ union_find.cpp
@@ -1,20 +1,20 @@
-// Union find with amortized O(1) union_set() and find_set()
 #include <iostream>
 using namespace std;
-#define MAXN (int)1e5 + 1
+#define MAXN int(1e5) + 1
 
+///
 int parent[MAXN];
 int ranks [MAXN];
 
-void prepare() {
+void prepare_sets() {
 	for (int i = 0; i < MAXN; ++i) {
-		parent[i] = -1;
-		ranks [i] =  1;
+		parent[i] = i;
+		ranks [i] = 1;
 	}
 }
 
 int find_set(int i) {
-	if (parent[i] == -1)
+	if (parent[i] == i)
 		return i;
 	return parent[i] = find_set(parent[i]);
 }
@@ -31,12 +31,14 @@ void union_sets(int a, int b) {
 	parent[b] = a; // B is attached to A
 	ranks[a] += ranks[b];
 }
+///
 
 int main()
 {
-	prepare();
-	union_sets(1 , 3);
-	if (find_set(1) == find_set(3)) {
-		cout << "1 and 3 are in the set \"" << find_set(1) << "\"\n";
-	}
+	prepare_sets();
+	union_sets(1 , 2); // { 1 , 2 }
+	union_sets(2 , 3); // { 1 , 2 , 3 }
+
+	if (find_set(1) == find_set(3))
+		printf("1 and 3 are in the same set (%d)\n" , find_set(1));
 }
